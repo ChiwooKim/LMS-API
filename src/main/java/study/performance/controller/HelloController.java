1005abcb22174ff3b97aa077b81f2e2b2fa3588f
ChiwooKim <kcw0360@naver.com>
2024-08-25 16:36:14 +0900

    Feat: "HelloController" 생성"
    간단한 테스트용 컨트롤러 및 dto 생성

---
+++ src/main/java/study/performance/controller/HelloController.java
@@ -0,0 +1,20 @@
+package study.performance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import study.performance.dto.response.HelloResponseDto;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
