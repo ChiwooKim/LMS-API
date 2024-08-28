075d0cb73b73952d7db8f98730d24dc5472ecce0
ChiwooKim <kcw0360@naver.com>
2024-08-28 23:46:25 +0900

    test: "공연정보 수정/삭제 api 테스트"
    공연정보 수정 및 삭제 api 테스트코드 작성 후 테스트

--- src/test/java/study/performance/controller/AdminControllerTest.java
+++ src/test/java/study/performance/controller/AdminControllerTest.java
@@ -9,10 +9,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import study.performance.domain.Performance;
 import study.performance.dto.request.PerformanceInformationRequestDto;
+import study.performance.dto.request.PerformanceInformationUpdateDto;
 import study.performance.repository.PerformanceRepository;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -63,12 +68,104 @@ class AdminControllerTest {
         String json = objectMapper.writeValueAsString(requestDto);
 
         //expected
-        mockMvc.perform(MockMvcRequestBuilders.post("/admin/")
+        mockMvc.perform(post("/admin/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(json)
                 )
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("공연 정보 수정")
+    void test2() throws Exception {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        String updateName = "뮤지컬 시카고 <창원>";
+        PerformanceInformationUpdateDto updateDto = PerformanceInformationUpdateDto.builder()
+                .performanceName(updateName)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+
+        String json = objectMapper.writeValueAsString(updateDto);
+
+        //when
+        mockMvc.perform(patch("/admin/{performanceId}", performance.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        List<Performance> list = performanceRepository.findAll();
+        assertThat(list.getFirst().getPerformanceName()).isNotEqualTo(name);
+        assertThat(list.getFirst().getPerformanceName()).isEqualTo(updateName);
+    }
+
+    @Test
+    @DisplayName("공연 정보 삭제")
+    void test3() throws Exception {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        //when
+        mockMvc.perform(delete("/admin/{performanceId}", performance.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        assertThat(performanceRepository.findById(performance.getId())).isEmpty();
     }
 }
--- src/test/java/study/performance/service/AdminServiceTest.java
+++ src/test/java/study/performance/service/AdminServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.performance.domain.Performance;
 import study.performance.dto.request.PerformanceInformationRequestDto;
+import study.performance.dto.request.PerformanceInformationUpdateDto;
 import study.performance.repository.PerformanceRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -59,5 +60,87 @@ class AdminServiceTest {
         Performance performance = performanceRepository.findAll().getFirst();
         assertThat(performance.getPerformanceName()).isEqualTo(name);
         assertThat(performance.getPerformanceNumber()).isEqualTo(number);
+    }
+
+    @Test
+    @DisplayName("공연 정보 수정")
+    void test2() {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        String updateName = "뮤지컬 시카고 <창원>";
+        PerformanceInformationUpdateDto updateDto = PerformanceInformationUpdateDto.builder()
+                .performanceName(updateName)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+
+        //when
+        adminService.update(performance.getId(), updateDto);
+
+        //then
+        Performance updatePerformance = performanceRepository.findById(performance.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 공연이 존재하지 않습니다. id=" + performance.getId()));
+        assertThat(updatePerformance.getPerformanceName()).isNotEqualTo(name);
+        assertThat(updatePerformance.getPerformanceName()).isEqualTo(updateName);
+    }
+
+    @Test
+    @DisplayName("공연 정보 삭제")
+    void test3() {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        //when
+        adminService.delete(performance.getId());
+
+        //then
+        assertThat(performanceRepository.count()).isEqualTo(0);
     }
 }
