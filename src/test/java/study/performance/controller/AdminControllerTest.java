4f00fa5b6302e1efcaf33bc0029cddd65cf72fe2
ChiwooKim <kcw0360@naver.com>
2024-08-31 10:11:59 +0900

    Test: "관리자 페이지 공연정보 단건 조회 api test"
    하나의 공연정보를 불러오는 기능 테스트

--- src/test/java/study/performance/controller/AdminControllerTest.java
+++ src/test/java/study/performance/controller/AdminControllerTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -167,5 +168,40 @@ class AdminControllerTest {
 
         //then
         assertThat(performanceRepository.findById(performance.getId())).isEmpty();
+    }
+
+    @Test
+    @DisplayName("1개의 영화 정보 조회")
+    void test4() throws Exception {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        //expected
+        mockMvc.perform(get("/admin/{performanceId}", performance.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.performanceName").value(performance.getPerformanceName()))
+                .andDo(print());
     }
 }
--- src/test/java/study/performance/service/AdminServiceTest.java
+++ src/test/java/study/performance/service/AdminServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import study.performance.domain.Performance;
 import study.performance.dto.request.PerformanceInformationRequestDto;
 import study.performance.dto.request.PerformanceInformationUpdateDto;
+import study.performance.dto.response.PerformanceInformationResponseDto;
 import study.performance.repository.PerformanceRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -142,5 +143,48 @@ class AdminServiceTest {
 
         //then
         assertThat(performanceRepository.count()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("1개의 영화 정보 조회")
+    void test4() {
+        //given
+        String name = "시카고";
+        String number = "24011991";
+        String location = "창원 성산아트홀 대극장";
+        String startDate = "2024.10.25";
+        String endDate = "2024.10.27";
+        String runtime = "145분(인터미션 20분 포함)";
+        String age = "14세 이상 관람가";
+        String price = "VIP석 160,000원\n" + "R석 140,000원\n" + "S석 110,000원\n" + "A석 80,000원\n";
+        String thumbnail = "//ticketimage.interpark.com/Play/image/large/24/24011991_p.gif";
+        Performance performance = Performance.builder()
+                .performanceName(name)
+                .performanceNumber(number)
+                .performanceLocation(location)
+                .startDate(startDate)
+                .endDate(endDate)
+                .runtime(runtime)
+                .age(age)
+                .price(price)
+                .thumbnail(thumbnail)
+                .build();
+        performanceRepository.save(performance);
+
+        //when
+        PerformanceInformationResponseDto response = adminService.get(performance.getId());
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(performanceRepository.count()).isEqualTo(1);
+        assertThat(response.getPerformanceName()).isEqualTo(name);
+        assertThat(response.getPerformanceNumber()).isEqualTo(number);
+        assertThat(response.getPerformanceLocation()).isEqualTo(location);
+        assertThat(response.getStartDate()).isEqualTo(startDate);
+        assertThat(response.getEndDate()).isEqualTo(endDate);
+        assertThat(response.getRuntime()).isEqualTo(runtime);
+        assertThat(response.getAge()).isEqualTo(age);
+        assertThat(response.getPrice()).isEqualTo(price);
+        assertThat(response.getThumbnail()).isEqualTo(thumbnail);
     }
 }
